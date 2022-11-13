9946fcbcc137a4ea13e78e908017a9b3f03f97e3
AlexanderCH23 <118133626+AlexanderCH23@users.noreply.github.com>
2022-11-13 15:40:33 -0500

    llll4

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ print("En efecto")
 print("no le sabes")
 print("ssssssssssss")
 print("no se")
+
+fj = input("escribe un numero ")
+
+print("ceo de ")
+
+def hermanos_():
+    print("hola")
+hermanos_()
+print("holassssss")
