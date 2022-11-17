d4096e7dd7a22f20822449dee55757c9b3ef8b6a
Sofoclesias <isoschronos@gmail.com>
2022-11-17 17:12:14 -0500

    Menú de inicio - bosquejo

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
-print("hola mundo")
-print("En efecto")
-print("no le sabes")
-print("ssssssssssss")
-print("no se")
+print('  BIENVENIDO  '.center(50, '#'))
+while True:
+    op = int(input('''
+    1. Registro
+    2. Portal de compras
+    3. Portal administrativo
+    4. Salir
 
-fj = input("escribe un numero ")
-
-print("ceo de ")
-
-def hermanos_():
-    print("hola")
-hermanos_()
-print("holassssss")
-
-print("Fuente? De cebiche")
-
-print("uwu")
-x = int("45615161")
-
-print("Wenas")
+    Selecciona tu opción: '''))
+    print('\n', '#' * 50)
+    if op == 1:
+        print('Registro')
+    elif op == 2:
+        print('''Portal de compras''')
+    elif op == 3:
+        print('''Portal de administradores''')
+    elif op == 4:
+        break
+    else:
+        print(' Ingresa una opcion del menú')
