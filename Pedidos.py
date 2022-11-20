82de0161ecc4143a0575aea4ab530fcde51275ab
Suntales <118133581+Suntales@users.noreply.github.com>
2022-11-20 01:10:23 -0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.idea/workspace.xml

--- Pedidos.py
+++ Pedidos.py
@@ -1,3 +1,5 @@
+carritoprod = []
+carritoext = []
 prod = [[1, "Pizza americana", 24.90], [2, "Pizza hawaiana", 26.90], [3, "Pizza suprema", 29.90],
         [4, "Pizza cuatro quesos", 28.90],
         [5, "Pizza de pepperoni", 27.90]]
@@ -7,16 +9,36 @@ extras = [[1, "tomate", 1.90], [2, "parmesano", 2.90], [3, "bbq", 3.90], [4, "an
 
 
 def realizarpedidos():
-    print("¿Qué producto desea ordenar?")
     print("Menú Productos".center(31, "*"))
     print("{0:<4}{3}{1:<19}{3}{2:^6}".format("ID", "Productos", "Precios", "|"))
     for i in prod:
         print("{0:<4}{3}{1:<19}{3}{2:>6}".format(i[0], i[1], i[2], "|"))
-    print("¿Qué extras desea añadir?")
+    print("Una vez terminado, escriba: salir")
+    while True:
+        opcprod = input("¿Qué producto desea ordenar? ")
+        if opcprod == "1":
+            carritoprod.append(prod[1][1])
+        elif opcprod == "2":
+            carritoprod.append(prod[2][1])
+        elif opcprod == "salir":
+            break
+        else:
+            print("Ingresar un codigo válido")
+
     print("Menú Extras".center(31, "*"))
     print("{0:4}{3}{1:<19}{3}{2:^6}".format("ID", "Productos", "Precios", "|"))
     for i in extras:
         print("{0:<4}{3}{1:<19}{3}{2:>6}".format(i[0], i[1], i[2], "|"))
+    print("Una vez terminado, escriba: salir")
+    while True:
+        opcext = input("¿Qué extras desea añadir? ")
+        if opcext == "1":
+            carritoext.append(extras[1][1])
+        elif opcext == "salir":
+            break
+        else:
+            print("Ingresar un codigo válido")
+    print("Pizzas: ", carritoprod, "Extras: ", carritoext)
 
 
 realizarpedidos()
