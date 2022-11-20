23915c5ced92abe213a7192e07dc633a331fddf3
Suntales <118133581+Suntales@users.noreply.github.com>
2022-11-19 22:37:14 -0500

    Tarjetas2

--- Tarjetas.py
+++ Tarjetas.py
@@ -1,10 +1,28 @@
-T = []
+Tar = []
 
 
-def Tarjetas(tarjeta):
+def tarjetas():
     while True:
         tarjeta = input("Ingrese los 16 caracteres de su tarjeta: ")
+        tarjeta4 = list(tarjeta)
+        dig = tarjeta4[-4:]
         if len(tarjeta) == 16:
-            T.append(tarjeta)
+            Tar.append(tarjeta)
+            print("Tarjeta añadida")
+            print("**** **** **** ", dig)
+            print("¿Desea confirmar la tarjeta?")
+            print("1. Sí")
+            print("2. No")
+            opc = input("")
+            if opc == "1":
+                print(Tar)
+                break
+            elif opc == "2":
+                Tar.remove(tarjeta)
+                print(Tar)
+                pass
         else:
             print("Ingrese un número válido")
+
+
+tarjetas()
