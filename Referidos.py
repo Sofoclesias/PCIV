4f816117c8584cd124736f28da1ee13dc608b819
Suntales <118133581+Suntales@users.noreply.github.com>
2022-11-20 12:51:53 -0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.idea/workspace.xml

--- Referidos.py
+++ Referidos.py
@@ -5,7 +5,7 @@ numero_referido = int(clientela[i][8])
 for i in range(len(clientela)):
     if numero_referido > 0:
         clientela[i][8] = int(clientela[i][8]) - 1
-        precio = precio - 0.20
+        precio = precio - 0.50
         pass
     else:
         print("No cuenta con puntos de referido")
