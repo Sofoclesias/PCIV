45d65b0d066c178a440c2ecb8f610de419fe42f5
Suntales <118133581+Suntales@users.noreply.github.com>
2022-11-19 23:30:57 -0500

    Pedidos5

--- Pedidos.py
+++ Pedidos.py
@@ -1,11 +1,12 @@
-prod = ["Pizza americana", "Pizza hawaiana", "Pizza suprema", "Pizza cuatro quesos", "Pizza de pepperoni"]
-extra = ["tomate", "anchoas", "piña", "bbq", "bordes de queso"]
+prod = [["Pizza americana", 24.90], ["Pizza hawaiana", 26.90], ["Pizza suprema", 29.90], ["Pizza cuatro quesos", 28.90],
+        ["Pizza de pepperoni", 27.90]]
+extras = ["tomate", "anchoas", "piña", "bbq", "bordes de queso"]
 
 
 def realizarpedidos():
     print("Menú".center(28, "*"))
-    for x in prod:
-        print("{}")
+    for i in prod:
+        print("{0}".format(prod[i][0]))
 
 
 realizarpedidos()
