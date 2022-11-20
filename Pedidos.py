b3f4f1d8463222e5348cc0dc401b6032ba1b50c9
Suntales <118133581+Suntales@users.noreply.github.com>
2022-11-20 00:38:04 -0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.idea/workspace.xml

--- Pedidos.py
+++ Pedidos.py
@@ -1,14 +1,21 @@
-prod = [["Pizza americana", 24.90], ["Pizza hawaiana", 26.90], ["Pizza suprema", 29.90], ["Pizza cuatro quesos", 28.90],
-        ["Pizza de pepperoni", 27.90]]
-prod1 = ["Pizza americana", "Pizza hawaiana", "Pizza suprema", "Pizza cuatro quesos",
-         "Pizza de pepperoni"]
-extras = ["tomate", "anchoas", "piña", "bbq", "bordes de queso"]
+prod = [[1, "Pizza americana", 24.90], [2, "Pizza hawaiana", 26.90], [3, "Pizza suprema", 29.90],
+        [4, "Pizza cuatro quesos", 28.90],
+        [5, "Pizza de pepperoni", 27.90]]
+extras = [[1, "tomate", 1.90], [2, "parmesano", 2.90], [3, "bbq", 3.90], [4, "anchoas", 5.90], [5, "piña", 4.90],
+          [6, "chorizo", 4.90], [7, "jamón", 4.90], [8, "bordes de queso", 5.90], [9, "pepperoni", 5.90],
+          [10, "champiññones", 6.90], [11, "salchicha", 7.90]]
 
 
 def realizarpedidos():
-    print("Menú".center(28, "*"))
+    print("Menú Productos".center(36, "*"))
+    print("{0:<4}{1:<19}{2:^19}".format("ID", "Productos", "Precios"))
     for i in prod:
-        print("{0}".format(prod[i][0]))
-
+        print("{0:4}{1:<19}{2:^19}".format(i[0], i[1], i[2]))
+    print("Menú Extras".center(36, "*"))
+    print("{0:4}{1:<19}{2:^19}".format("ID", "Productos", "Precios"))
+    for i in extras:
+        print("{0:4}{1:<19}{2:^19}".format(i[0], i[1], i[2]))
+    while True:
+        pass
 
 realizarpedidos()
