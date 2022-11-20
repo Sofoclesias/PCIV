6724fb505f65743a8f738c64c8dfa89baa8d0555
AlexanderCH23 <118133626+AlexanderCH23@users.noreply.github.com>
2022-11-19 23:47:57 -0500

    actualizando cosas1234

--- Tarjetas.py
+++ Tarjetas.py
@@ -1,21 +1,36 @@
 Tar = []
-
-
 def tarjetas():
     while True:
-        tarjeta = int(input("Ingrese los 16 caracteres de su tarjeta: "))
+        while True:
+            try:
+                tarjeta = input("Ingrese los 16 caracteres de su tarjeta: ")
+                if int(tarjeta) > 0 and len(tarjeta) == 16:
+                    break
+                else:
+                    print("escriba una tarjeta verdadera")
+            except:
+                print("escriba una tarjeta verdadera")
+                pass
         if len(str(tarjeta)) == 16:
             Tar.append(tarjeta)
-            print("Tarjeta añadida")
-            print("**** **** **** ", str(tarjeta)[-4:])
-            print("¿Desea confirmar la tarjeta?")
-            print("1. Sí")
-            print("2. No")
-            opc = input("")
-            if opc == "1":
-                print(Tar)
+            while True:
+                try:
+                    print("Tarjeta añadida")
+                    print("**** **** **** ", str(tarjeta)[-4:])
+                    print("¿Desea confirmar la tarjeta?")
+                    print("1. Sí")
+                    print("2. No")
+                    opc = int(input(""))
+                    if str(opc) in "12":
+                        break
+                    else:
+                        print("escriba una opcion válida")
+                except:
+                    print("escriba una opción valida")
+
+            if opc == 1:
                 break
-            elif opc == "2":
+            elif opc == 2:
                 Tar.remove(tarjeta)
                 print(Tar)
             else:
