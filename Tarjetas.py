b8b7f8ad2244be03555704c109eb018e316efac0
Suntales <118133581+Suntales@users.noreply.github.com>
2022-11-19 22:15:33 -0500

    Menú de inicio - bosquejo

--- Tarjetas.py
+++ Tarjetas.py
@@ -4,7 +4,7 @@ T = []
 def Tarjetas(tarjeta):
     while True:
         tarjeta = input("Ingrese los 16 caracteres de su tarjeta: ")
-        if len(tarjeta) = 16:
+        if len(tarjeta) == 16:
             T.append(tarjeta)
         else:
             print("Ingrese un número válido")
