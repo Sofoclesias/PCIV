d0c16333617a7002b6c778a4ebed86f698f01193
Suntales <118133581+Suntales@users.noreply.github.com>
2022-11-20 01:16:01 -0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.idea/workspace.xml

--- Pedidos.py
+++ Pedidos.py
@@ -1,5 +1,6 @@
 carritoprod = []
 carritoext = []
+precio = []
 prod = [[1, "Pizza americana", 24.90], [2, "Pizza hawaiana", 26.90], [3, "Pizza suprema", 29.90],
         [4, "Pizza cuatro quesos", 28.90],
         [5, "Pizza de pepperoni", 27.90]]
@@ -16,10 +17,9 @@ def realizarpedidos():
     print("Una vez terminado, escriba: salir")
     while True:
         opcprod = input("¿Qué producto desea ordenar? ")
-        if opcprod == "1":
-            carritoprod.append(prod[1][1])
-        elif opcprod == "2":
-            carritoprod.append(prod[2][1])
+        if opcprod == i[0]:
+            carritoprod.append(prod[i][1])
+            precio.append(prod[i][3])
         elif opcprod == "salir":
             break
         else:
@@ -32,13 +32,14 @@ def realizarpedidos():
     print("Una vez terminado, escriba: salir")
     while True:
         opcext = input("¿Qué extras desea añadir? ")
-        if opcext == "1":
-            carritoext.append(extras[1][1])
+        if opcext == i[0]:
+            carritoext.append(extras[i][1])
+            precio.append(extras[i][3])
         elif opcext == "salir":
             break
         else:
             print("Ingresar un codigo válido")
-    print("Pizzas: ", carritoprod, "Extras: ", carritoext)
+    print("Pizzas: ", carritoprod, "Extras: ", carritoext, "Precio:", precio)
 
 
 realizarpedidos()
